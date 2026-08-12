2cd9c7a334c936a7ee893a212af09f8f227f8cbb
Thapelo <tmatabane952@gmail.com>
2026-08-12 11:49:02 +0200

    basic hovering works without use of different colours

---
+++ etch.js
@@ -0,0 +1,34 @@
+const cont = document.querySelector(".container");
+let originalWidth = getComputedStyle(cont).width;
+
+
+// for (let i = 0; i < 16; i++){
+//     const newDiv = document.createElement("div");
+//     cont.appendChild(newDiv);
+// }
+
+function changeGrid(){
+    cont.replaceChildren();
+
+    let squares = parseInt(prompt("enter an int to change grid: "));
+     while (squares < 16 || squares > 100){
+        squares = parseInt(prompt("enter an int to change grid: "));
+    }
+    cont.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
+    cont.style.gridTemplateRows = `repeat(${squares},1fr)`;
+    const totalSquares = squares * squares;
+    for (let i = 0; i < totalSquares;i++){
+        const cell = document.createElement('div');
+        cell.classList.add('grid-cell');
+
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = '#333333'; // Draws black
+    });
+
+    cont.appendChild(cell);
+  }
+}
+
+
+ const btn = document.querySelector("#btn");
+ btn.onclick = changeGrid;
